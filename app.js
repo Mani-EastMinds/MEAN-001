4e8b87f399eae2abfcbe9b94030e62f955894665
Mani SRIPATHI <admin@eastminds.com>
2017-05-26 21:55:57 -0500

    Made changes to messages route in nodejs

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
@@ -38,7 +38,7 @@ app.use('/', appRoutes);
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     return res.render('index');
 });
 
--- routes/messages.js
+++ routes/messages.js
@@ -2,126 +2,123 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 
+var User = require('../models/user');
 var Message = require('../models/message');
 
-router.get('/', function(req, res, next){
-  Message.find()
-    .exec(function(err, messages) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An Error Occured',
-          error: err
+router.get('/', function(req, res, next) {
+    Message.find()
+        .exec(function(err, messages) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An Error Occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: messages
+            });
         });
-      }
-      res.status(200).json({
-        message: 'Success',
-        obj: messages
-      });
-    });
 });
 
 router.use('/', function(req, res, next) {
-  jwt.verify(req.query.token, 'secret', function(err, decoded) {
-    if (err) {
-      return res.status(401).json({
-        title: 'Not Authenticated!',
-        error: err
-      });
-      next();
-    }
-  });
+    jwt.verify(req.query.token, 'secret', function(err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                title: 'Not Authenticated!',
+                error: err
+            });
+        }
+        next();
+    });
 });
 
 router.post('/', function(req, res, next) {
-  var decoded = jwt.decode(req.query.token);
-  User.findById(decoded.user._id, function(err, user) {
-    if (err) {
-      return res.status(500).json({
-        title: 'An Error Occured',
-        error: err
-      });
-    }
-
-    var message = new Message({
-      content: req.body.content
-    });
-    message.save(function(err, result) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An Error Occured',
-          error: err
+    var decoded = jwt.decode(req.query.token);
+    User.findById(decoded.user._id, function(err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An Error Occured',
+                error: err
+            });
+        }
+        var message = new Message({
+            content: req.body.content
         });
-      }
-      res.status(201).json({
-        message: 'Saved Message!',
-        obj: result
-      });
+        message.save(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An Error Occured',
+                    error: err
+                });
+            }
+            user.messages.push(result);
+            user.save();
+            res.status(201).json({
+                message: 'Saved Message!',
+                obj: result
+            });
+        });
     });
-  });
 });
 
 router.patch('/:id', function(req, res, next) {
-  Message.findById(req.params.id, function(err, message){
-    if (err) {
-      return res.status(500).json({
-        title: 'An Error Occured',
-        error: err
-      });
-    }
-
-    if (!message) {
-      return res.status(500).json({
-        title: 'No Message Found!',
-        error: {message: 'Message not found'}
-      });
-    }
-
-    message.content = req.body.content;
-    message.save(function(err, result) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An Error Occured',
-          error: err
+    Message.findById(req.params.id, function(err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An Error Occured',
+                error: err
+            });
+        }
+        if (!message) {
+            return res.status(500).json({
+                title: 'No Message Found!',
+                error: { message: 'Message not found' }
+            });
+        }
+        message.content = req.body.content;
+        message.save(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An Error Occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Updated Message!',
+                obj: result
+            });
         });
-      }
-      res.status(200).json({
-        message: 'Updated Message!',
-        obj: result
-      });
     });
-  });
 });
 
 router.delete('/:id', function(req, res, next) {
-  Message.findById(req.params.id, function(err, message) {
-    if (err) {
-      return res.status(500).json({
-        title: 'An Error Occured',
-        error: err
-      });
-    }
-
-    if (!message) {
-      return res.status(500).json({
-        title: 'No Message Found!',
-        error: {message: 'Message not found'}
-      });
-    }
-
-    message.remove(function(err, result) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An Error Occured',
-          error: err
+    Message.findById(req.params.id, function(err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An Error Occured',
+                error: err
+            });
+        }
+        if (!message) {
+            return res.status(500).json({
+                title: 'No Message Found!',
+                error: { message: 'Message not found' }
+            });
+        }
+        message.remove(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An Error Occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Deleted Message!',
+                obj: result
+            });
         });
-      }
-      res.status(200).json({
-        message: 'Deleted Message!',
-        obj: result
-      });
     });
-
-  });
 })
 
 module.exports = router;
